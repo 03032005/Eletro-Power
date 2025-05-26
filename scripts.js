865cc5bedc467527c9e7b2c5ef854540fc4e33a2
03032005 <richardasilva2005@gmail.com>
2025-05-26 12:34:43 -0300

    Primeiro commit - Projeto Eletro Power

---
+++ scripts.js
@@ -0,0 +1,16 @@
+ let formulario = document.querySelector('.formulario');
+ let mascara = document.querySelector('.mascara-form');
+
+  function mostrarForm() {
+    formulario.style.right = '200px'
+    mascara.style.visibility = 'visible'
+ }
+
+
+
+ function esconderForm() {
+    formulario.style.right = '-507px';
+      mascara.style.visibility = 'hidden'
+ }
+
+ console.loig("ola mundo")
